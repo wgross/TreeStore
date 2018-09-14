2c714f485574eb0b1dec278ccfdfcd51b647a812
Wolfgang Gross <github@text-mode.de>
2018-09-14 23:40:30 +0200

    entity list is now a user control

---
+++ Kosmograph.Desktop/Controls/ShowEntitiesControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Kosmograph.Desktop.ViewModel;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Kosmograph.Desktop.Controls
+{
+    public partial class ShowEntitiesControl : UserControl
+    {
+        public ShowEntitiesControl()
+        {
+            this.InitializeComponent();
+        }
+
+        private KosmographViewModel ViewModel => this.DataContext as KosmographViewModel;
+
+        private void entityListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e) => this.ViewModel.EditEntityCommand.Execute(this.ViewModel.SelectedEntity);
+    }
+}
