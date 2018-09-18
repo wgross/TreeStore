06404c594b89730f012e88f53eeab1aa0b7d35a9
Wolfgang Gross <github@text-mode.de>
2018-09-18 18:33:49 +0200

    added teg editor control; stripped edit resources

---
+++ Kosmograph.Desktop/Controls/EditorHeaderControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Kosmograph.Desktop.Controls
+{
+    public partial class EditorHeaderControl : UserControl
+    {
+        public static readonly DependencyProperty TextProperty;
+
+        static EditorHeaderControl()
+        {
+            TextProperty = TextBlock.TextProperty.AddOwner(typeof(EditorHeaderControl), new FrameworkPropertyMetadata { Inherits = true });
+        }
+
+        public EditorHeaderControl()
+        {
+            this.InitializeComponent();
+        }
+
+        public string Text
+        {
+            get => (string)this.GetValue(TextProperty);
+            set => this.SetValue(TextProperty, value);
+        }
+    }
+}
