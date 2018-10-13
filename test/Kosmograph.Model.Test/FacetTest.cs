bb65d09bddfc99e8de9ae2ed9d254e02f0b2eace
Wolfgang Gross <github@text-mode.de>
2018-10-13 16:42:09 +0200

    duplicate property is rejected because of duplicate name now

--- test/Kosmograph.Model.Test/FacetTest.cs
+++ test/Kosmograph.Model.Test/FacetTest.cs
@@ -36,25 +36,6 @@ namespace Kosmograph.Model.Test
         }
 
         [Fact]
-        public void Facet_ignores_duplicate_property()
-        {
-            // ARRANGE
-
-            var facet = new Facet();
-            var property = new FacetProperty();
-
-            facet.AddProperty(property);
-
-            // ACT
-
-            facet.AddProperty(property);
-
-            // ASSERT
-
-            Assert.Equal(property, facet.Properties.Single());
-        }
-
-        [Fact]
         public void Facet_removes_Property()
         {
             // ARRANGE
