5a4d43dcb69bd85f7004e5abe0306d551896eacd
Wolfgang Gross <github@text-mode.de>
2018-11-02 10:24:01 +0100

    swapping 'from' and 'to' in relationship editor with DnD

--- src/Kosmograph.Desktop/Controls/EditRelatedEntitiesControl.xaml.cs
+++ src/Kosmograph.Desktop/Controls/EditRelatedEntitiesControl.xaml.cs
@@ -7,6 +7,9 @@ namespace Kosmograph.Desktop.Controls
 {
     public partial class EditRelatedEntitiesControl : UserControl
     {
+        private bool toDropAreaMouseDown;
+        private bool fromDropAreaMouseDown;
+
         public EditRelatedEntitiesControl()
         {
             this.InitializeComponent();
@@ -18,7 +21,12 @@ namespace Kosmograph.Desktop.Controls
         {
             if (e.Data.GetDataPresent(typeof(EntityViewModel)))
             {
+                var oldfrom = this.ViewModel.From;
                 this.ViewModel.From = (EntityViewModel)e.Data.GetData(typeof(EntityViewModel));
+                if (this.toDropAreaMouseDown)
+                    this.ViewModel.To = oldfrom;
+
+                this.fromDropAreaMouseDown = this.toDropAreaMouseDown = false;
             }
             e.Handled = true;
         }
@@ -27,9 +35,62 @@ namespace Kosmograph.Desktop.Controls
         {
             if (e.Data.GetDataPresent(typeof(EntityViewModel)))
             {
+                var oldTo = this.ViewModel.To;
                 this.ViewModel.To = (EntityViewModel)e.Data.GetData(typeof(EntityViewModel));
+                if (this.fromDropAreaMouseDown)
+                    this.ViewModel.From = oldTo;
+
+                this.fromDropAreaMouseDown = this.toDropAreaMouseDown = false;
             }
             e.Handled = true;
+        }
+
+        private void toDropArea_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton != System.Windows.Input.MouseButtonState.Pressed)
+            {
+                this.fromDropAreaMouseDown = this.toDropAreaMouseDown = false;
+            }
+            else
+            {
+                var relatsionhipEditModel = ((FrameworkElement)sender).DataContext as RelationshipEditModel;
+                if (relatsionhipEditModel is null)
+                    return;
+
+                DataObject data = new DataObject();
+                data.SetData(typeof(EntityViewModel), relatsionhipEditModel.To);
+
+                DragDrop.DoDragDrop((DependencyObject)sender, data, DragDropEffects.Link);
+            }
+        }
+
+        private void fromDropArea_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton != System.Windows.Input.MouseButtonState.Pressed)
+            {
+                this.fromDropAreaMouseDown = this.toDropAreaMouseDown = false;
+            }
+            else
+            {
+                var relatsionhipEditModel = ((FrameworkElement)sender).DataContext as RelationshipEditModel;
+                if (relatsionhipEditModel is null)
+                    return;
+
+                DataObject data = new DataObject();
+                data.SetData(typeof(EntityViewModel), relatsionhipEditModel.From);
+
+                DragDrop.DoDragDrop((DependencyObject)sender, data, DragDropEffects.Link);
+            }
+        }
+
+        private void toDropArea_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.toDropAreaMouseDown = true;
+        }
+
+        private void fromDropArea_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.fromDropAreaMouseDown = true;
         }
     }
 }
