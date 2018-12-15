55b5e67549d3758bf1d1c75f73e15872970643d6
Wolfgang Gross <github@text-mode.de>
2018-12-15 14:21:26 +0100

    Added test project for remoaining desktop parts

---
+++ test/Kosmograph.Desktop.Test/NullToVisiblityConverterTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using Xunit;
+
+namespace Kosmograph.Desktop.Test
+{
+    public class NullToVisiblityConverterTest
+    {
+        private readonly NullToVisiblityConverter converter;
+
+        public NullToVisiblityConverterTest()
+        {
+            this.converter = new NullToVisiblityConverter();
+        }
+
+        [Theory]
+        [InlineData(null, Visibility.Hidden)]
+        [InlineData((object)1, Visibility.Visible)]
+        public void Convert_to_Visibility(object value, Visibility expected)
+        {
+            // ACT
+
+            var result = this.converter.Convert(value, typeof(Visibility), null, null);
+
+            // ASSERT
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
