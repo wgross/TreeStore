aa430227c3229777fba7677a8f7c073a5a77f3fc
Wolfgang Gross <github@text-mode.de>
2018-12-31 23:22:18 +0100

    Prepared events to handlet tag repo views interaction

--- src/Kosmograph.Desktop.Lists/View/TagRepositoryView.xaml.cs
+++ src/Kosmograph.Desktop.Lists/View/TagRepositoryView.xaml.cs
@@ -32,5 +32,96 @@ namespace Kosmograph.Desktop.Lists.View
                 DragDrop.DoDragDrop((DependencyObject)sender, data, DragDropEffects.Link);
             }
         }
+
+        #region Map keyboard input to routed events
+
+        private void repositoryListBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Delete:
+                    this.RaiseDeleteTagEvent();
+                    e.Handled = true;
+                    break;
+
+                case Key.Return:
+                    this.RaiseEditTagEvent();
+                    e.Handled = true;
+                    break;
+
+                case Key.Insert:
+                    this.RaiseCreateTagEvent();
+                    e.Handled = true;
+                    break;
+            }
+        }
+
+        #endregion Map keyboard input to routed events
+
+        #region Map mpuse events to routed events
+
+        private void repositoryListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e) => this.RaiseEditTagEvent();
+
+        #endregion Map mpuse events to routed events
+
+        #region Request the editin of a Tag
+
+        public static readonly RoutedEvent EditTagEvent = EventManager.RegisterRoutedEvent("EditTag", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TagRepositoryView));
+
+        public event RoutedEventHandler EditTag
+        {
+            add
+            {
+                this.AddHandler(EditTagEvent, value);
+            }
+            remove
+            {
+                this.RemoveHandler(EditTagEvent, value);
+            }
+        }
+
+        private void RaiseEditTagEvent() => this.RaiseEvent(new RoutedEventArgs(EditTagEvent));
+
+        #endregion Request the editin of a Tag
+
+        #region Request the deletion of a Tag
+
+        public static readonly RoutedEvent DeleteTagEvent = EventManager.RegisterRoutedEvent("DeleteTag", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TagRepositoryView));
+
+        public event RoutedEventHandler DeleteTag
+        {
+            add
+            {
+                this.AddHandler(DeleteTagEvent, value);
+            }
+            remove
+            {
+                this.RemoveHandler(DeleteTagEvent, value);
+            }
+        }
+
+        private void RaiseDeleteTagEvent() => this.RaiseEvent(new RoutedEventArgs(DeleteTagEvent));
+
+        #endregion Request the deletion of a Tag
+
+        #region Request the creation of a Tag
+
+        public static readonly RoutedEvent CreateTagEvent = EventManager.RegisterRoutedEvent("CreateTag", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(TagRepositoryView));
+
+        public event RoutedEventHandler CreateTag
+        {
+            add
+            {
+                this.AddHandler(CreateTagEvent, value);
+            }
+            remove
+            {
+                this.RemoveHandler(CreateTagEvent, value);
+            }
+        }
+
+        private void RaiseCreateTagEvent() => this.RaiseEvent(new RoutedEventArgs(CreateTagEvent));
+
+        #endregion Request the creation of a Tag
     }
 }
